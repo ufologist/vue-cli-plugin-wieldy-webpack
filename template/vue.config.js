95405f6a1031bf4476f4443f42925b9be03177f3
ufologist <klsforever@gmail.com>
2019-07-16 11:42:29 +0800

    实现创建 page config 的便捷方法

--- template/vue.config.js
+++ template/vue.config.js
@@ -1,4 +1,5 @@
 var getPublicPath = require('vue-cli-plugin-wieldy-webpack/lib/get-public-path.js');
+var createPageConfig = require('vue-cli-plugin-wieldy-webpack/lib/create-page-config.js');
 
 module.exports = {
     publicPath: getPublicPath()
